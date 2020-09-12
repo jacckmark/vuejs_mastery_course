0d0e1684e3bfee69c93882deef9efdd3ff635caf
jacckmark@gmail.com <jacckmark@gmail.com>
2020-09-12 18:56:36 +0200

    first few lessons of module 02

---
+++ 02-real_world_vuejs/01/src/components/EventCart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        quote: "i dont care",
+      };
+    },
+  };
+</script>
+
+<style>
+</style>
