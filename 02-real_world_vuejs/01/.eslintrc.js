b87045158a78df56e541466183e578474abd6aa3
jacckmark@gmail.com <jacckmark@gmail.com>
2020-09-15 09:04:06 +0200

    lesson 4 and 5, changes to eslint and editor configuration

--- 02-real_world_vuejs/01/.eslintrc.js
+++ 02-real_world_vuejs/01/.eslintrc.js
@@ -3,12 +3,30 @@ module.exports = {
     env: {
         node: true
     },
-    extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended", "@vue/prettier"],
+    extends: [ "plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended", "@vue/prettier" ],
     parserOptions: {
         parser: "babel-eslint"
     },
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "vue/script-indent": [ "warn", 2, {
+            baseIndent: 1
+        } ],
+        "vue/html-indent": [ "warn", 2, {
+            baseIndent: 1
+        } ],
+        "no-extra-semi": "off",
+        "prettier/prettier": [
+            'warn',
+            {
+                tabWidth: 2,
+                useTabs: false,
+                bracketSpacing: true,
+                eslintIntegration: true,
+                vueIndentScriptAndStyle: true,
+                trailingComma: "all"
+            }
+        ]
     }
 };
---
+++ 02-real_world_vuejs/01/src/views/EventList.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Events Listing</h1>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+</style>
